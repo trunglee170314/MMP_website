ae68f24e89b9742849a85bbafe80ce06f3b8675d
levantrung <lezantrung@gmail.com>
2025-08-24 23:48:27 +0700

    app: boards: Add baseline boards APIs
    This commits supports the APIs as below:
    1. Get all board
    2. Get a board
    3. Add a board
    4. Delete a board
    5. Update a board
    6. Get my boards
    
    Note: relationshiop between Board and User table is many to many.
    Example: 1 user can hold many boards. 1 board is holded by many users.
    
    Signed-off-by: levantrung <lezantrung@gmail.com>

--- BE/app/boards/repository.py
+++ BE/app/boards/repository.py
@@ -1,5 +1,72 @@
 from typing import List, Optional
 from typing_extensions import Protocol
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import select
 from . import models
+from ..iam import models as user_models
+from ..iam.association import user_board
+from .schemas import OutBoard, InBoard
+
+class BoardRepository(Protocol):
+    def list_by_user(self, user_id: int) -> List[models.Board]: ...
+    def get_by_barcode(self) -> Optional[models.Board]: ...
+    def list_all_board(self) -> List[models.Board]: ...
+    def get_by_id(self, board_id: int) -> Optional[models.Board]: ...
+    def add(self, payload: InBoard, owners: List[user_models.User]) -> models.Board: ...
+    def update(self, board: models.Board, payload: InBoard, owners: List[user_models.User]) -> models.Board: ...
+    def delete(self, board: models.Board) -> None: ...
+
+class SqlAlchemyBoardRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_barcode(self, barcode: str) -> Optional[models.Board]:
+        stmt = select(models.Board).where(models.Board.barcode == barcode)
+        return self.db.execute(stmt).scalars().first()
+
+    def list_by_user(self, user_id: int) -> List[models.Board]:
+        stmt = (select(models.Board)
+        .join(models.user_board, models.Board.id == models.user_board.c.board_id)
+        .where(models.user_board.c.user_id == user_id)
+        .options(selectinload(models.Board.owners))
+        )
+        return self.db.scalars(stmt).all()
+
+    def list_all_board(self) -> List[models.Board]:
+        return self.db.scalars(
+            select(models.Board).options(selectinload(models.Board.owners))
+            ).all()
+
+    def get_by_id(self, board_id: int) -> Optional[models.Board]:
+        b = self.db.get(models.Board, board_id)
+        return b
+
+    def add(self, payload: InBoard, owners: List[user_models.User]) -> models.Board:
+        b = models.Board(
+            board_name = payload.board_name,
+            barcode = payload.barcode,
+            ip = payload.ip,
+            note = payload.note or "",
+            status = payload.status,
+            location = payload.location or "",
+            owners = list(owners)
+            )
+
+        self.db.add(b)
+        self.db.commit()
+        self.db.refresh(b)
+        return b
+
+    def update(self, board: models.Board, payload: InBoard, owners: List[user_models.User]) -> models.Board:
+        data = payload.dict(exclude_unset=True)
+        for field, value in data.items():
+            setattr(board, field, value)
+
+        board.owners = list(owners)
+
+        self.db.commit(); self.db.refresh(board)
+        return board
+
+    def delete(self, board: models.Board) -> None:
+        self.db.delete(board)
+        self.db.commit()
--- BE/app/boards/service.py
+++ BE/app/boards/service.py
@@ -1,3 +1,60 @@
-from typing import Optional
-from .repository import BoardRepository
+from typing import Optional, List
+from fastapi import HTTPException
+
 from . import models
+from ..iam import models as user_models
+from ..iam.repository import UserRepository
+from .repository import BoardRepository
+from .schemas import InBoard
+
+class BoardService:
+    def __init__(self, repo: BoardRepository, user_repo: UserRepository):
+        self.repo = repo
+        self.user_repo = user_repo
+
+    # helpers
+    def _found_board(self, b: models.Board) -> models.Board:
+        if not b:
+            raise ValueError("Board not found")
+        return b
+
+    def _found_user(self, u: user_models.User) -> user_models.User:
+        if not u:
+            raise ValueError("User not found")
+        return u
+
+    # actions
+    def list_all_board(self) -> List[models.Board]:
+        return self.repo.list_all_board()
+
+    def list_my_boards(self, user_id: int):
+        return self.repo.list_by_user(user_id)
+
+    def get_by_id(self, board_id: int) -> models.Board:
+        return self._found_board(self.repo.get_by_id(board_id))
+
+    def add_board(self, payload: InBoard) -> models.Board:
+        if self.repo.get_by_barcode(payload.barcode):
+            raise ValueError("Board already existed")
+        owners = self.user_repo.get_by_ids(payload.owner_ids)
+        if len(owners) != len(set(payload.owner_ids)):
+            raise ValueError("Some owner_ids not found")
+
+        return self.repo.add(payload, owners)
+
+    def update_board(self, board_id: int, payload: InBoard) -> models.Board:
+        board = self._found_board(self.repo.get_by_id(board_id))
+
+        if payload.barcode and payload.barcode != board.barcode:
+            if self.repo.get_by_barcode(payload.barcode):
+                raise ValueError("Barcode already exists")
+
+        owners = self.user_repo.get_by_ids(payload.owner_ids)
+        if len(owners) != len(set(payload.owner_ids)):
+            raise ValueError("Some owner_ids not found")
+
+        return self.repo.update(self.get_by_id(board_id), payload, owners)
+
+    def delete_board(self, board_id: int) -> None:
+        b = self.get_by_id(board_id)
+        self.repo.delete(b)
